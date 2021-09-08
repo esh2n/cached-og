981958a43a165af2ca9ea46138426cd7e834f6b3
Shunya Endo <shunyaendoh.bz@gmail.com>
2021-09-08 14:01:43 +0900

    ✨ feat: add ogp

---
+++ src/components/common/OpenGraph/OpenGraph.tsx
@@ -0,0 +1,118 @@
+import Logo from '@components/ui/Logo';
+export interface Props {
+  title: string;
+  body: string;
+  color: string;
+  tags: string[];
+}
+
+const OpenGraph = ({ title, body, color, tags }: Props) => {
+  const css = `
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@700&display=swap');
+    html,
+    body {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    .wrapper {
+      width: 1200px;
+      height: 630px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      position: relative;
+      font-family: 'M PLUS Rounded 1c';
+      font-weight: bold;
+      background-color: #ffffff;
+    }
+    .header {
+      padding: 80px;
+      display: flex;
+      justify-content: space-between;
+    }
+    .content {
+      display: flex;
+      flex-direction: column;
+      width: 750px;
+
+    }
+    .title {
+      font-size: 50px;
+      color: #2E3438;
+      display: -webkit-box;
+      overflow: hidden;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+    .body {
+      padding-top: 40px;
+
+      font-size: 20px;
+      color: #828688;
+      display: -webkit-box;
+      overflow: hidden;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+    }
+    .author {
+      font-size: 30px;
+      font-weight: 700;
+    }
+
+    .footer {
+      padding: 80px;
+      display: flex;
+      justify-content: space-between;
+      color: #2E3438
+    }
+    .tags {
+      display: flex;
+    }
+    .tags--item {
+      font-size: 30px;
+      margin-right: 48px;
+    }
+    .bar {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 32px;
+      background-color: #DCA584;
+    }
+  `;
+
+  return (
+    <html>
+      <style dangerouslySetInnerHTML={{ __html: css }} />
+      <body>
+        <div className="wrapper">
+          <div className="header">
+            <div className="content">
+              <div className="title">{title}</div>
+              <div className="body">{body}</div>
+            </div>
+            <Logo height={200} width={200} />
+          </div>
+
+          <div className="footer">
+            <div className="tags">
+              {tags.map((e, i) => (
+                <div key={i} className="tags--item">
+                  {e}
+                </div>
+              ))}
+            </div>
+
+            <div className="author">@esh2n</div>
+          </div>
+
+          <div className="bar" style={{ backgroundColor: color }}></div>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default OpenGraph;
---
+++ src/pages/api/og/[...tags].ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import * as playwright from 'playwright-aws-lambda';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import OpenGraph from '@components/common/OpenGraph/OpenGraph';
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isPreview = typeof process.env.BLOG_PREVIEW !== 'undefined';
+
+async function getLaunchOptions() {
+  if (isDev || isPreview) {
+    return {
+      args: [],
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      headless: true,
+    };
+  } else {
+    return {};
+  }
+}
+
+function getHtml({ title, body, color, tags }: Content): string {
+  const element = React.createElement(OpenGraph, { title, body, color, tags });
+  const markup = ReactDOMServer.renderToStaticMarkup(element);
+
+  return `<!doctype html>${markup}`;
+}
+export interface Content {
+  title: string;
+  body: string;
+  color: string;
+  tags: string | string[];
+}
+
+function getContent(req: NextApiRequest): Content {
+  const title = req.query.title as string;
+  const body = req.query.body as string;
+  const color = req.query.body as string;
+  const { tags } = req.query;
+  return { title, body, color, tags };
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const content = getContent(req);
+    console.log(content);
+
+    const html = getHtml(content);
+    const viewport = { width: 1200, height: 630 };
+    const launchOptions = await getLaunchOptions();
+    const browser = await playwright.launchChromium(launchOptions);
+    const page = await browser.newPage({ viewport });
+
+    await page.setContent(html, { waitUntil: 'networkidle' });
+    const buffer = await page.screenshot({ type: 'png' });
+    await browser.close();
+
+    res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate');
+    res.setHeader('Content-Type', 'image/png');
+
+    res.end(buffer);
+  } catch (error) {
+    console.error('[Error]: ', error);
+    res.status(404).json({ message: 'Cannot render og-image' });
+  }
+};
